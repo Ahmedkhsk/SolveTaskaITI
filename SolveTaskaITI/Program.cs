c31ca0aac1d34d42dac31f40b878b3bebd9199bf
Ahmedkhsk <mrahmedkhaled2004@gmail.com>
2025-09-11 19:57:44 +0300

    Add Day1

--- SolveTaskaITI/Program.cs
+++ SolveTaskaITI/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Day 1 Tasks
+            Day1.CharToAscii();
+            Day1.AsciiToChar();
+            Day1.CheckOddEven();
+            Day1.BasicOperations();
+            Day1.CalculateGrade();
+            #endregion
+
+            #region Day 2 Tasks
+            #endregion
+
         }
     }
 }
