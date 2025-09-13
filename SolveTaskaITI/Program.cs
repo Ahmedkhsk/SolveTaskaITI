403aef8ea0132449672d5aea4ea4d3daee65955b
Ahmedkhsk <mrahmedkhaled2004@gmail.com>
2025-09-13 19:33:15 +0300

    Remove Comments

--- SolveTaskaITI/Program.cs
+++ SolveTaskaITI/Program.cs
@@ -5,25 +5,25 @@ using SolveTaskaITI.Day3;
 
 #region Day 1 Tasks
 
-    //Day1.CharToAscii();
-    //Day1.AsciiToChar();
-    //Day1.CheckOddEven();
-    //Day1.BasicOperations();
-    //Day1.CalculateGrade();
+    Day1.CharToAscii();
+    Day1.AsciiToChar();
+    Day1.CheckOddEven();
+    Day1.BasicOperations();
+    Day1.CalculateGrade();
 
 #endregion
 
 #region Day 2 Tasks
 
-    //Day2.InputPrintStudentNames();
-    //Day2.InputPrintCalculateAverageStudentAges();
-    //Day2.TimeToSeconds();
+    Day2.InputPrintStudentNames();
+    Day2.InputPrintCalculateAverageStudentAges();
+    Day2.TimeToSeconds();
 
 #endregion
 
 #region Day 3 Tasks
 
-//Day3.Calc();
-Day3.Question();
+    Day3.Calc();
+    Day3.Question();
 
 #endregion
